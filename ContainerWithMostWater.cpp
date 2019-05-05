a9d2459a1a89505816e471efb26b9bd48a7277a7
Gaurav Singh <49317238+itsgauravsingh@users.noreply.github.com>
2019-05-05 22:46:03 +0530

    Create ContainerWithMostWater.cpp
    https://leetcode.com/problems/container-with-most-water/

---
+++ ContainerWithMostWater.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int a = 0;
+        int r = height.size()-1;
+        int l=0;
+        while(l<r) {
+            a = a>(r-l)*(height[r]>height[l]? height[l]:height[r])?a:(r-l)*(height[r]>height[l]? height[l]:height[r]);
+            height[r]>height[l] ? l++ : r--;
+        }
+        return a;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> height = stringToIntegerVector(line);
+        
+        int ret = Solution().maxArea(height);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
